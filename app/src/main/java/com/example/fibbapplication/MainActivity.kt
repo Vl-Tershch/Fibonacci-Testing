7b2a5264e314d71fa7095d1d5ef3c92d327d5b82
Vl-Tershch <ddiavall@yandex.ru>
2022-04-16 15:31:09 +0300

    Changed the structure and added tests

--- app/src/main/java/com/example/fibbapplication/MainActivity.kt
+++ app/src/main/java/com/example/fibbapplication/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,22 +15,24 @@ class MainActivity : AppCompatActivity() {
         val textInput = findViewById<EditText>(R.id.editText)
         val textRez = findViewById<TextView>(R.id.textView2)
         button.setOnClickListener {
-            var rez = Fibonnachi(textInput.text.toString().toInt())
+            val rez = FibMethod().Fibonnacci(textInput.text.toString().toInt())
             textRez.setText(rez.toString())
         }
     }
+}
 
-    fun Fibonnachi(num: Int):Int {
+class FibMethod {
+    fun Fibonnacci(num: Any):Any {
         var f0 = 0
         var f1 = 1
         var c = 2
-        if (num == 0)
+        if (num.toString().toInt() == 0)
             return 0
         else if (num == 1)
             return 1
         else {
-            while (c < num) {
-                var help = f1 + f0
+            while (c <= num.toString().toInt()) {
+                val help = f1 + f0
                 f0 = f1
                 f1 = help
                 c += 1
--- app/src/test/java/com/example/fibbapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/fibbapplication/ExampleUnitTest.kt
@@ -11,7 +11,32 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun zero_isCorrerct() {
+        assertEquals(0, FibMethod().Fibonnacci(0))
+    }
+
+    @Test
+    fun integers_areCorrect1() {
+        assertEquals(2, FibMethod().Fibonnacci(3))
+    }
+
+    @Test
+    fun integers_areCorrect2() {
+        assertEquals(34, FibMethod().Fibonnacci(9))
+    }
+
+    @Test
+    fun negative_isCorrerct() {
+        assertEquals(-1, FibMethod().Fibonnacci(-1))
+    }
+
+    @Test
+    fun letters_areCorrerct() {
+        assertEquals("Error, not integers", FibMethod().Fibonnacci("n"))
+    }
+
+    @Test
+    fun doubles_areCorrerct() {
+        assertEquals("Error, not integers", FibMethod().Fibonnacci(3.1))
     }
 }
